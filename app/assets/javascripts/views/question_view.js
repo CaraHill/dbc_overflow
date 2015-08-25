13521c8a2b48c828c3bed658bc113485c98f7d61
Cara Hill <chilly23@gmail.com>
2015-08-25 16:54:33 +1200

    Moved the answers code from the show event/ajax call in questions.js into the show method in question_view, so it has all the appropriate info even if it is not yet working properly.

--- app/assets/javascripts/views/question_view.js
+++ app/assets/javascripts/views/question_view.js
@@ -52,6 +52,14 @@ QuestionView.prototype = {
     var questionDiv = '<div class="question" data-question-id="'+questionId+'">'+question.content+' - '+question.user_name+' <a class="delete-button" href="">Delete</a> '+'<div class="new-answer"><a class="create-answer-button" href="">Answer this Question</a>'+'<form class="new-answer-form" method="post" action="/questions/'+questionId+'/answers"><input type="text" name="answer[content]" placeholder="Answer a Question"><input type="submit" value="Submit Answer"></form>'+' <a class="answers-button" href="">See Answers</a>'+'<div class="answers-go-here"></div>'+'</div>'
     $('#dbc_stack').empty();
     $('#dbc_stack').append(questionDiv);
+    var answersDiv = $('#dbc_stack').find('.answers-go-here');
+    var answers = data.answer
+    for(var i=0; i< answers.length; i++) {
+      var answer = answers[i];
+      var answerId = answer.id;
+      var answerDiv = '<div class="question-answers" data-answer-id="'+answerId+'">'+answer.content+' - '+answer.user_name+' <a class="answer-delete-button" href="">Delete</a>'+'</div>'
+      answersDiv.append(answerDiv);
+    }
     QuestionView.hideQuestionDiv;
     QuestionsView.allQuestionsDiv;
   },
