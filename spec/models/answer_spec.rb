e158f0a6b88549b8ac951b1526b80a2691025f58
Cara Hill <chilly23@gmail.com>
2015-08-26 15:47:09 +1200

    Added tests for the Answer model.

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates an instance of answer" do
+    answer = Answer.new
+    expect(answer).to be_instance_of(Answer)
+  end
+
+  describe "answer validity" do
+    it "creates a valid answer" do
+      answer = Answer.new(content: "Hello!")
+      expect(answer).to be_valid
+    end
+  end
 end
