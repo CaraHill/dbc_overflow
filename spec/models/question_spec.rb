c32686e074a11eaa490495c416fa5fd4ac216c3b
Cara Hill <chilly23@gmail.com>
2015-08-26 15:59:16 +1200

    Added a before do to the Quesiton Model specs.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
-
 RSpec.describe Question, type: :model do
 
+  before do
+    @question = Question.create(content: "This is a question?")
+  end
+
   it "creates an instance of question" do
-    question = Question.new
-    expect(question).to be_instance_of(Question)
+    expect(@question).to be_instance_of(Question)
   end
 
   describe "question validity" do
-    it "create a valid Question" do
-      question = Question.new(content: "Hello!")
-      expect(question).to be_valid
+    it "creates a valid Question" do
+      expect(@question).to be_valid
     end
   end
 end
