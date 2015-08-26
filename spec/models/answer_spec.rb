d57b19b514af39b5f74952bc63d4fcb4e9e7df84
Cara Hill <chilly23@gmail.com>
2015-08-26 15:56:31 +1200

    Added a before do to the Answer Model specs.

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+
+  before do
+    @answer = Answer.create(content: "Hello!")
+  end
+
   it "creates an instance of answer" do
-    answer = Answer.new
-    expect(answer).to be_instance_of(Answer)
+    expect(@answer).to be_instance_of(Answer)
   end
 
   describe "answer validity" do
     it "creates a valid answer" do
-      answer = Answer.new(content: "Hello!")
-      expect(answer).to be_valid
+      expect(@answer).to be_valid
     end
   end
 end
