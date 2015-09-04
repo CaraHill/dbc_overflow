7d3e99ed319524b064791527e11027ba10724667
Cara Hill <chilly23@gmail.com>
2015-09-04 14:18:41 +1200

    Further refactored the remove action in the questions controller.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -30,22 +30,14 @@ class QuestionsController < ApplicationController
 
   def remove
     authenticate_user!
-    question = Question.find(params[:id])
+    if current_user.id == question.user_id
+      question = Question.find(params[:id])
+    else
+      render status: 400, json: {
+        message: "Your request was not successful. Please try again."
+      }
+    end
 
-    # nested if-else is a bit of a code smell. It increases the no. of possible
-    # paths of execution in your method. A good way to visualise this is to
-    # draw a flow chart
-    # (https://en.wikipedia.org/wiki/Flowchart#/media/File:LampFlowchart.svg)
-    # of the possible pathways through this method - notice the effect that
-    # each if-else has on the graph. When you are testing a method you will
-    # want to (eventually) have tests that trigger all of these possible paths.
-    # If you are anything like me, you will notice that the no. of possible
-    # paths through your method is higher than you think!
-    #
-    # All this makes if-else something we want to refactor away from. Note that
-    # refactoring does not change the no. of possible paths of execution - it
-    # just surfaces them better for our brains to reason about.
-    #
     remove_question_answers(question)
 
     if question.destroy
@@ -66,13 +58,6 @@ class QuestionsController < ApplicationController
   end
 
   def remove_question_answers(question)
-    authenticate_user!
-    if current_user.id == question.user_id
-      Answer.where(question_id: question.id).destroy_all
-    else
-      render status: 400, json: {
-        message: "Your request was not successful. Please try again."
-      }
-    end
+    Answer.where(question_id: question.id).destroy_all
   end
 end
